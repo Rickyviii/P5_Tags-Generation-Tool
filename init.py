8a08e37c4f60949abc39afcc270fe53806dd4c42
rickyviii <richard.courtillon@gmail.com>
2021-08-18 18:32:50 +0200

    Templates and CSS files _ first try

--- init.py
+++ init.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__) #Flask application instance
 
 @app.route('/') #app.route transform the output of the function into an HTTP response when the URL is / (root of the web site)
 def question():
-    return ('Please input here your question (limited to 255 characters):')
+    return render_template('main_template.html')
