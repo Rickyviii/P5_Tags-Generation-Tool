a7cdfeb095c221edf520c17f122fb79ae0b7e473
rickyviii <richard.courtillon@gmail.com>
2021-08-19 12:26:25 +0200

    user question input

--- init.py
+++ init.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__) #Flask application instance
 
+length_question = 150
+
 @app.route('/') #app.route transform the output of the function into an HTTP response when the URL is / (root of the web site)
 def question():
-    return render_template('main_template.html')
+    return render_template('main_template.html', len_qu = length_question)
