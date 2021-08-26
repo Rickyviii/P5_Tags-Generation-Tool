29cc43d2c1953d32cde78db07067ced4b49bde86
rickyviii <richard.courtillon@gmail.com>
2021-08-26 13:17:54 +0200

    Addition of LDA unsupervised model, better error handling

--- functions.py
+++ functions.py
@@ -1,11 +1,15 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import MultiLabelBinarizer
 import re
+import time
+import pandas as pd
+
 import nltk
 nltk.download('stopwords')
 nltk.download('wordnet')
-import time
-import pandas as pd
+
+############################ ----------- Functions for document processing
+# ----------- (preprocessing, tokenizing, lemming, stemming, postprocessing...) ----------- ############################
 
 #remove stop words within a list
 def remove_stop_word(list_, stopwords):
@@ -57,7 +61,7 @@ def stemming_list(List_, stemmer):
 
 
 
-################################# ----------- CountVectorizer Functions -------------- ############################
+############################ ----------- CountVectorizer Functions ----------- ############################
 
 #preprocessing of the document includes just lowerization
 def my_preprocessor(doc):
@@ -89,16 +93,14 @@ def my_tokenizer(s):
     Step6 = remove_stop_word(Step5, list_stop_words_2)
     return Step6
 
-
 def tokenize_question(input_string):
     #Vectorization
     CVect = CountVectorizer(preprocessor = my_preprocessor, tokenizer = my_tokenizer, stop_words = None, token_pattern = None)
 
-    #CVect.fit_transform([input_string])
     try:
         CVect.fit_transform([input_string])
-    except Exception as e:
-        return str(e)
+    except Exception as err:
+        return str(err)
     else:
         #output as a list of cleaned stemmed words
         return CVect.get_feature_names()
@@ -107,7 +109,7 @@ def create_dummy_1R_matrix(input_list_tags, trained_list_words): #create input f
     mlb0 = MultiLabelBinarizer()
     mlb0.fit([trained_list_words])
     df_input_for_model = pd.DataFrame(mlb0.transform([input_list_tags]))
-    df_input_for_model.columns=trained_list_words
+    df_input_for_model.columns = trained_list_words
     return df_input_for_model
 
 #function which returns the column name each time value is 1 in row (input list)
@@ -115,35 +117,68 @@ def get_column_name(list01, list_columns):
     list_indx = [i for i, e in enumerate(list01) if e == 1]
     return [list_columns[i] for i in  list_indx]
 
+############################ ----------- unsupervised approach only ----------- ############################
 
-################################# ----------- Create_tags final Function -------------- ############################
+#function which select best x topics for new user input, and then retrieve best words (tags) associated to the selected topics
+def retrieve_tags_from_user_input(model, input_question_tokenized, trained_list_words, nb_topics = 1, nb_words = 3):
 
-def create_tags(input_user, model, words_list_for_training, tag_list):
+    Matrix_1row_question_topics = model.transform(input_question_tokenized) # 1 row doc-topics matrix
+    df_userquestion_topics = pd.DataFrame(Matrix_1row_question_topics) #doc_topics matrix
+
+    df_topic_words = pd.DataFrame(model.components_) #topic_words matrix
+    print('topic words shape', df_topic_words.shape)
+    print('doc topics shape', df_userquestion_topics.shape)
+    #indices of best topics to consider for the document (question) index
+    best_topics_ix = df_userquestion_topics.loc[0,:].argsort()[:-1-nb_topics:-1].values.tolist() #nb_topics top topics
+
+    Tags_=[]
+    for best_topic in best_topics_ix:
+        print('****** LOOP ****** ')
+
+        Tag_indices = df_topic_words.loc[best_topic,:].argsort()[:-1-nb_words:-1].values.tolist() #nb_words top words
+
+        print ('best_topic:', best_topic, '_ tag_indices: ', Tag_indices,'__ type: ', type(Tag_indices) )
+        Tags_ = Tags_ + [trained_list_words[tag] for tag in Tag_indices]
+
+    return Tags_ #returns a list of best tags
+
+############################ ----------- Create_tags final Function ----------- ############################
+
+def create_tags(input_user, model, words_list_for_training, tag_list, unsupervised = False):
+    #Step1 _ preprocessing, tokenizing, post processing
     st = time.time()
     input_tokenized = tokenize_question(input_user)
     if isinstance(input_tokenized, str):
-        return ("error_at_Step_1 (tokenization): " + input_tokenized) #error and exit
-    print('step1 completed _ tokenized input:', input_tokenized)
+        print('Error at step 1: ' + input_tokenized)
+        return "ERR1", input_tokenized, True
+    else:
+        print('step1 completed _ tokenized input:', input_tokenized)
 
-    #Step2 _ here, we create an entry which actually a tf matrix for one question, based on vocabulary list (words list) used for training
+    #Step2 _ here, we create an entry which is actually a tf matrix for one question,
+            # based on vocabulary list (words list) used for training
     input_dummy = create_dummy_1R_matrix(input_tokenized, words_list_for_training)
     try:
         print('step2 completed _ tokenized dummy input:\n', input_dummy[input_tokenized], '_ shape:', input_dummy.shape)
-    except Exception as e:
-        return ("error_at_Step_2: " + str(e))
+    except Exception as err:
+        return "ERR2", str(err), True #True means error
 
     try:
-        #Step3 _ we use the model to make a prediction on the input computed at step 2
-        output_dummy_Tags = model.predict(input_dummy)[0].tolist()
-        print('step3 completed _ dummy tags output: size = 1 x', len(output_dummy_Tags))
+        if unsupervised == False: #semi supervised or fully supervised model -> we use a classifier prediction
+            #Step3a _ we use the model to make a prediction on the input computed at step 2
+            output_dummy_Tags = model.predict(input_dummy)[0].tolist()
+            print('step3 completed _ dummy tags output: size = 1 x', len(output_dummy_Tags))
 
-        #Step4 _ we retrieve the tags from the model 1R vector output
-        output_tags = get_column_name(output_dummy_Tags, tag_list)
-        e = (time.time() - st)/60
+            #Step4 _ we retrieve the tags from the model 1R vector output
+            output_tags = get_column_name(output_dummy_Tags, tag_list)
+            print('step4 completed _ output tags:', output_tags)
+        else:
+            #Step3b _ we use directly the dimension reduction algorithm to extract main words (tags)
+            output_tags = retrieve_tags_from_user_input(model, input_dummy, words_list_for_training, nb_topics = 1, nb_words = 3)
+            print('step3b completed _ output tags:', output_tags)
 
-        #Step4
-        print('step4 completed _ output tags:', output_tags)
-        print('time elapsed: {0:.2f} min.'.format(e))
-        return output_tags, e
-    except Exception as e:
-        return ("This error occured:" + str(e))
+        te = (time.time() - st)/60
+        print('time elapsed: {0:.2f} min.'.format(te))
+        return output_tags, te, False #False means no error
+
+    except Exception as err:
+        return "UNKNOWN_ERROR", str(err), True #True means there is an error
--- init.py
+++ init.py
@@ -5,16 +5,20 @@ import functions
 import pandas as pd
 
 app = Flask(__name__) #Flask application instance
+
+print(__name__)
 max_length_question = 150
 
 
 #import of various tag models
 ThisDir = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(ThisDir, r"files\models\stovf_model.pkl") ,"rb") as f:
-    stovf_tag_model = pickle.load(f)
 
-with open(os.path.join(ThisDir, r"files\models\nmf_model_2.pkl"),   "rb") as f:
+with open(os.path.join(ThisDir, r"files\models\lda_unsupervised_model.pkl"),   "rb") as f:
+    lda_tag_model   = pickle.load(f)
+with open(os.path.join(ThisDir, r"files\models\nmf_LSVC_semi_supervised_model.pkl"),   "rb") as f:
     nmf_tag_model   = pickle.load(f)
+with open(os.path.join(ThisDir, r"files\models\stovf_LSVC_supervised_model.pkl") ,"rb") as f:
+    stovf_tag_model = pickle.load(f)
 
 #import of list of words used to train the models
 list_words      = pd.read_csv(r'files\CSV files\words.csv', keep_default_na=False).word.tolist()
@@ -25,40 +29,51 @@ list_tags_stovf = pd.read_csv(r'files\CSV files\St_OVF_Tag_list.csv', keep_defau
 #main HTML page generation
 @app.route('/', methods=['POST', 'GET']) #app.route transform the output of the function into an HTTP response when the URL is / (root of the web site)
 def question():
+    ERR = False
     if request.method == 'POST':
         form_data = request.form
         input_user = list(form_data.values())[0] #question from the input_user (string)
         model = list(form_data.values())[1] #model selected by user
-        if model in ['NMF', 'STOVF']:
-            if model == "NMF":
+
+        if model in ['LDA', 'NMF', 'STOVF']:
+            if model == "LDA":
+                tag_model = lda_tag_model
+                list_tags = ''
+                unsup = True
+            elif model == "NMF":
                 tag_model = nmf_tag_model
                 list_tags = list_tags_nmf
+                unsup = False
             elif model == "STOVF":
                 tag_model = stovf_tag_model
                 list_tags = list_tags_stovf
+                unsup = False
             #computation and output
-            list_tags_output_ = functions.create_tags(input_user, tag_model, list_words, list_tags)
+            list_tags_output_ = functions.create_tags(input_user, tag_model, list_words, list_tags, unsup)
             print('***********************', list_tags_output_)
-            if isinstance(list_tags_output_, str):  #ERROR
-                output_to_user = 'Unknown error'
-                err = ['error_at_Step_1', 'error_at_Step_2', 'This error occured:']
-                for i,e_ in enumerate(err):
-                    if e_ in list_tags_output_:
-                        if i<2:
-                            output_to_user = e_
-                        else:
-                            output_to_user = list_tags_output_
-                        break #'exit for' if one known error has been matched
-            else:
+            if list_tags_output_[2] == True:  #ERROR
+                ERR = list_tags_output_[0]
+                ERR_MSG = list_tags_output_[1]
+                if ERR=="ERR1":
+                    err_msg ="The question does not contain any word specific enough. Please rephrase your question."
+                elif ERR=="ERR2":
+                    err_msg ="Vocabulary unknown. Please enter a valid question."
+                else:
+                    err_msg = "Unknown error.: '" + ERR_MSG + "'_ Please try again."
+                output_to_user = err_msg
+                ERR = True
+            else: #NO ERROR
                 output_to_user1 = "    " + "    ".join(['<'+c+'>' for c in list_tags_output_[0]]) #list of tags
                 output_to_user2 = "{0:.2f} min.".format(list_tags_output_[1]) #time elapsed
                 output_to_user = [output_to_user1, output_to_user2]
         else:
+            ERR = True
             output_to_user = "no_model"
+
         print('output', output_to_user)
         return render_template('main_template.html', len_qu = max_length_question,
-                                input_data = input_user, output_data = output_to_user, input_model = model)
+                                input_data = input_user, output_data = output_to_user, input_model = "", ERR = ERR)
 
     else: #REQUEST == GET
         return render_template('main_template.html', len_qu = max_length_question,
-                                input_data = 'your question', output_data = "", input_model="")
+                                input_data = 'your question', output_data = "", input_model="", ERR = ERR)
